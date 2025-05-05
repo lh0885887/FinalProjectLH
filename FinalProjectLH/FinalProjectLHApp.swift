3d1f08b566e241823a59332b812ecc8b1c518223
Logan Hill <lh0885887@otc.edu>
2025-05-05 08:12:30 -0500

    Initial Commit

---
+++ FinalProjectLH/FinalProjectLHApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectLHApp.swift
+//  FinalProjectLH
+//
+//  Created by HILL, LOGAN on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectLHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
